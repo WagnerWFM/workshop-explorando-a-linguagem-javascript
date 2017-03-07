a323112c26bd55b79df3c757bcd40f4e00d7d179
Wagner M <wagnercomputacao.unitri@yahoo.com.br>
2017-03-07 00:22:33 -0300

    typeof dos tipos em javascript

---
+++ type of/typeof.js
@@ -0,0 +1,25 @@
+// tipos de dados no JavaScript.
+
+var m = 'AlgaWorks';
+console.log('M:',typeof m); // string
+
+var n = 12;
+console.log('N:', typeof n); // number
+
+var ativo = true;
+console.log('Ativo:',typeof ativo);	// boolean
+
+var obj = {};
+console.log('Objeto:',typeof obj); // object
+
+var array = [];
+console.log('Array:',typeof array);	// object
+
+var nulo = null;
+console.log('Nulo:', typeof null);	// object
+
+var indefinido = undefined;
+console.log('Indefinido:', indefinido);	// undefined
+
+var f = function(){};
+console.log('Função:', typeof f);	// function
